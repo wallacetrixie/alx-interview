dbacfb7ae8e8cc96de7f53db6c0e72b9896b6025
wallace wambulwa <wallacewambulwa@gmail.com>
2024-08-06 06:42:12 -0700

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-'''A module for working with lockboxes.
-'''
+"""Minimum Operations"""
 
 
-def canUnlockAll(boxes):
-    '''Checks if all the boxes in a list of boxes containing the keys
-    (indices) to other boxes can be unlocked given that the first
-    box is unlocked.
-    '''
-    n = len(boxes)
-    seen_boxes = set([0])
-    unseen_boxes = set(boxes[0]).difference(set([0]))
-    while len(unseen_boxes) > 0:
-        boxIdx = unseen_boxes.pop()
-        if not boxIdx or boxIdx >= n or boxIdx < 0:
-            continue
-        if boxIdx not in seen_boxes:
-            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
-            seen_boxes.add(boxIdx)
-    return n == len(seen_boxes)
+def primeFactorization(x):
+    """Returns prime factorization elements of x"""
+    div = 2
+    array = list()
+    while (div <= x):
+        if x % div == 0:
+            array.append(div)
+            x /= div
+        else:
+            div += 1
+
+    return array
+
+
+def minOperations(n):
+    """Calculates the fewest number of operations needed
+        to result in exactly n H characters in the file"""
+    min = 0
+    factors = [x for x in primeFactorization(n)]
+    occurences = {item: factors.count(item) for item in factors}
+    for k, v in occurences.items():
+        min += k * v
+    return min
